be15cfcb40c5199423f943b65018114448e90b5b
Rpayet <robinpayet@gmail.com>
2024-08-26 10:36:02 +0200

    Fix username uniqueness validation in registerUser validator & Add Regex for password

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -1,7 +1,6 @@
 import User from '#models/user'
 import { loginUser, registerUser } from '#validators/auth'
 import type { HttpContext } from '@adonisjs/core/http'
-import hash from '@adonisjs/core/services/hash'
 
 export default class AuthController {
 
--- app/validators/auth.ts
+++ app/validators/auth.ts
@@ -4,12 +4,16 @@ const password = vine.string().minLength(8).maxLength(30)
 
 export const registerUser = vine.compile(
     vine.object({
-        username: vine.string().minLength(6).maxLength(20).optional(),
+        username: vine.string().minLength(6).maxLength(20).unique(async (db, value) => {
+            const match = await db.from('users').select('id').where('username', value).first()
+            return !match
+        }),
         email: vine.string().email().normalizeEmail().unique(async (db, value) => {
             const match = await db.from('users').select('id').where('email', value).first()
             return !match
         }),
-        password: vine.string().minLength(8).maxLength(30),
+        password: vine.string().minLength(8).maxLength(30).confirmed().regex(
+            /[a-z]/).regex(/[A-Z]/).regex(/[0-9]/).regex(/[^a-zA-Z0-9]/),
         picture: vine.string().optional()
     })
 )
